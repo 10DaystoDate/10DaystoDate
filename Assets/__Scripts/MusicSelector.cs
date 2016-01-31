471b37f1d60535bf89f376bda73b2c13940771b2
lloydfloyd <ryanpearce08@gmail.com>
2016-01-31 14:03:14 -0500

    music selector completed

---
+++ Assets/__Scripts/MusicSelector.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicSelector : MonoBehaviour {
+	public AudioClip[] bgMusic; 
+
+	// Use this for initialization
+	void Start () {
+
+		GetComponent<AudioSource>().clip = bgMusic[Random.Range(0,bgMusic.Length)];
+		GetComponent<AudioSource>().Play();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
